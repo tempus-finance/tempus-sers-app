6d47cab8fb93010dc6c4685984e33696891812c9
Yuval Weiss <yuval.weiss.ace@gmail.com>
2021-10-14 07:52:16 -0600

    wip

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
 export const whitelistUri = "https://gateway.pinata.cloud/ipfs/QmNgUbD4ao8aNWoNehUdzxMbdtXDymQByHiYPKsKpxz5mh";
+export const tempusSersContractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
