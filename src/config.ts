2178961e93ea6ba6ffe5c1a70f836918b6963e42
Lazar Mitic <Lazar__93@hotmail.com>
2021-10-21 20:14:32 +0200

    base uri update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const whitelistUris = ["https://ipfs.io/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh"];
 export const tempusSersContractAddress = "0x2e5ac807ed31c33db08d53c0b1b90547d5104e66";
-export const sersBaseUri = "https://ipfs.io/ipfs/QmYknWtV15cg2jLGAak4jXb67HPFpLZdNHneoCfhbFxL9i";
+export const sersBaseUri = "https://ipfs.io/ipfs/Qmbn13cvnneaovkWxGSaTfN2Wkjj6vW2JbqRNXWR2jTMhA";
