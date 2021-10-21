7fababa6cc39fe86e7c6d5234992ec02f3399340
Daniele Damiani <danieledamianiuk@gmail.com>
2021-10-21 17:18:16 +0100

    Add Num of Sers minted

--- src/components/RecentlyMintedSers/RecentlyMintedSers.tsx
+++ src/components/RecentlyMintedSers/RecentlyMintedSers.tsx
@@ -1,34 +1,32 @@
-import { FC, useCallback, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
 import SerCard from './SerCard';
 
 import './RecentlyMintedSers.css';
 import getSersDataProvider from '../../services/getSersDataProvider';
-import { JsonRpcProvider, Web3Provider } from '@ethersproject/providers';
 import { ethers } from 'ethers';
 
-const sers = [
-  { id: 1, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 2, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 3, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 4, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 5, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 6, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 7, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 8, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 9, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-  { id: 10, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
-];
+// const sers = [
+//   { id: 1, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 2, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 3, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 4, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 5, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 6, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 7, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 8, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 9, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+//   { id: 10, address: '0xAFE0B5E1bF4b9230A53e4A4715074ABf5B45F5de' },
+// ];
 
 const RecentlyMintedSers: FC = () => {
   const [recentlyMintedSers, setRecentlyMintedSers] = useState<any[]>([]);
-  
+
   useEffect(() => {
     async function fetchRecentlyMintedSers() {
-      const response = await getSersDataProvider(new ethers.providers.Web3Provider(
-        (window as any).ethereum,
-        'any'
-      )).getRecentlyMintedSers();
+      const response = await getSersDataProvider(
+        new ethers.providers.Web3Provider((window as any).ethereum, 'any')
+      ).getRecentlyMintedSers();
       setRecentlyMintedSers(response);
     }
     if (recentlyMintedSers.length === 0) {
@@ -36,10 +34,11 @@ const RecentlyMintedSers: FC = () => {
     }
   }, [recentlyMintedSers, setRecentlyMintedSers]);
 
-
   return (
     <div className="recently-minted-sers">
-      <h2>Recently Born Sers</h2>
+      <h2>Recently Born Sers </h2>
+      <h3>Total number of Sers minted</h3>
+      <h3>{recentlyMintedSers.length} out of 3333</h3>
       <Grid
         justifyContent="flex-start"
         alignItems="center"
@@ -51,11 +50,17 @@ const RecentlyMintedSers: FC = () => {
             <SerCard id={ser.id} address={ser.address} />
           </Grid>
         ))} */}
-        {recentlyMintedSers.map((ser: { tokenId: number; tokenUri: string; mintedTo: string }) => (
-          <Grid item xs={3} key={ser.tokenId}>
-            <SerCard id={ser.tokenId} address={ser.mintedTo} imageUri={ser.tokenUri} />
-          </Grid>
-        ))}
+        {recentlyMintedSers.map(
+          (ser: { tokenId: number; tokenUri: string; mintedTo: string }) => (
+            <Grid item xs={3} key={ser.tokenId}>
+              <SerCard
+                id={ser.tokenId}
+                address={ser.mintedTo}
+                imageUri={ser.tokenUri}
+              />
+            </Grid>
+          )
+        )}
       </Grid>
     </div>
   );
