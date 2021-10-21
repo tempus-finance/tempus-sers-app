3c26a31c1c9ada3f5b745e1ea45a139023ced14c
Lazar Mitic <Lazar__93@hotmail.com>
2021-10-21 19:00:44 +0200

    Update config for mainnet deployment

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const whitelistUris = ["/whitelist0.json"];
 // export const whitelistUris = ["/whitelist0.json", "/whitelist1.json"];
-export const tempusSersContractAddress = "0x039557b8f8f53d863f534c4dff01d8a3467d26a0";
-export const sersBaseUri = "https://ipfs.io/ipfs/QmYknWtV15cg2jLGAak4jXb67HPFpLZdNHneoCfhbFxL9i";
+export const tempusSersContractAddress = "0x2e5ac807ed31c33db08d53c0b1b90547d5104e66";
+export const sersBaseUri = "https://ipfs.io/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh";
