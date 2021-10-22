aacbe89db60f6b37a58807f753ffd9f18b3e926d
Daniele Damiani <danieledamianiuk@gmail.com>
2021-10-22 16:29:32 +0100

    Hide RecentlyMintedSers if provider not available

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { Container } from '@mui/material';
 import './App.css';
 import Main from './components/Main/Main';
@@ -7,12 +7,14 @@ import Header from './components/Header/Header';
 import UserSers from './components/RecentlyMintedSers/UserSers';
 
 const App: FC = () => {
+  const isProviderAvailable = useMemo(() => (window as any).ethereum, []);
+
   return (
     <Container maxWidth="sm" className="main">
       <Header />
       <Main />
       <UserSers />
-      <RecentlyMintedSers />
+      {isProviderAvailable && <RecentlyMintedSers />}
     </Container>
   );
 };
