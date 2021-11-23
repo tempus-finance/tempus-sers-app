57c84ce51ed87ac81dfe902a4fb3c99cd3854858
Lazar Mitic <Lazar__93@hotmail.com>
2021-11-23 11:51:42 +0100

    Add new whitelist

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
-export const whitelistUris = ["https://ipfs.io/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh", "https://ipfs.io/ipfs/QmWvw6AkgMsBwfPZksqrTaxeQcENiKy4Axiu4dTtWrwJdQ"];
+export const whitelistUris = [
+  "https://ipfs.io/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh",
+  "https://ipfs.io/ipfs/QmWvw6AkgMsBwfPZksqrTaxeQcENiKy4Axiu4dTtWrwJdQ",
+  "https://ipfs.io/ipfs/QmU9BpWHy9Wz1GreoZeWVUUZCLcZNhtsRia47ShWZyLRBG"
+];
 export const tempusSersContractAddress = "0x2e5ac807ed31c33db08d53c0b1b90547d5104e66";
 export const sersBaseUri = "https://firebasestorage.googleapis.com/v0/b/tempus-sers.appspot.com/o";
 export const recentlyBornSersElementsNum = 12;
