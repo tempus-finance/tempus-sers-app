0bc863ef3d28296dcd5dc02539a5f4007be999f2
Lazar <Lazar__93@hotmail.com>
2022-05-31 13:04:14 +0200

    Update IPFS URLs

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const whitelistUris = [
-  "https://ipfs.io/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh",
-  "https://ipfs.io/ipfs/QmWvw6AkgMsBwfPZksqrTaxeQcENiKy4Axiu4dTtWrwJdQ",
-  "https://ipfs.io/ipfs/QmU9BpWHy9Wz1GreoZeWVUUZCLcZNhtsRia47ShWZyLRBG",
-  "https://ipfs.io/ipfs/QmQitVWRLDBSE2MpgBuncaiuvuCdjXxbMd6cfRn9yBqFEA"
+  "https://thesers.mypinata.cloud/ipfs/QmfHWueEgeFp1UeJgagqj1rBTQyckTnyM9BxAMzipR9fPh",
+  "https://thesers.mypinata.cloud/ipfs/QmWvw6AkgMsBwfPZksqrTaxeQcENiKy4Axiu4dTtWrwJdQ",
+  "https://thesers.mypinata.cloud/ipfs/QmU9BpWHy9Wz1GreoZeWVUUZCLcZNhtsRia47ShWZyLRBG",
+  "https://thesers.mypinata.cloud/ipfs/QmQitVWRLDBSE2MpgBuncaiuvuCdjXxbMd6cfRn9yBqFEA"
 ];
 export const tempusSersContractAddress = "0x2e5ac807ed31c33db08d53c0b1b90547d5104e66";
 export const sersBaseUri = "https://firebasestorage.googleapis.com/v0/b/tempus-sers.appspot.com/o";
